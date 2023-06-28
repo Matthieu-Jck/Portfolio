0e9c83daa79864e843b176d68bb08dc4b9d646ee
Matthieu Jacques <Matthieu.Jacques.2077@gmail.com>
2023-06-29 01:39:03 +0200

    gear test

--- js/home.js
+++ js/home.js
@@ -1,60 +1,49 @@
 'use strict';
 
-function home_animateds()
-{
-	let done = false;
-	let elements = document.querySelectorAll('#home_section .animated');
-	let section = document.querySelector('#home_section');
+function home_animateds() {
+    let done = false;
+    let elements = document.querySelectorAll('#home_section .animated');
+    let section = document.querySelector('#home_section');
 
-	async function animated_check()
-	{
-		if (!done && is_in_viewport(section))
-		{
-			for (let i of elements)
-			{
-				i.style.opacity = '1';
-				i.style.transform = 'translateY(0)';
-				await sleep(300);
-			}
+    async function animated_check() {
+        if (!done && is_in_viewport(section)) {
+            for (let i of elements) {
+                i.style.opacity = '1';
+                i.style.transform = 'translateY(0)';
+                await sleep(300);
+            }
 
-			done = true;
-		}
-	}
+            done = true;
+        }
+    }
 
-	window.addEventListener('scroll', (e) =>
-	{
-		animated_check();
-	});
+    window.addEventListener('scroll', (e) => {
+        animated_check();
+    });
 
-	window.addEventListener('resize', (e) =>
-	{
-		animated_check();
-	});
+    window.addEventListener('resize', (e) => {
+        animated_check();
+    });
 
-	animated_check();
+    animated_check();
 }
 
-function home_events()
-{
-	let particles_paused = false;
-	particlesJS.load('particles', 'resources/jsons/particles.json');
+function home_events() {
+    let rectContent = document.querySelector('#home_section .content').getBoundingClientRect();
+    document.querySelector('#home_section').style.minHeight = (rectContent.height + 90) + 'px';
 
-	window.addEventListener('scroll', (e) =>
-	{
-		if (pJSDom[0] && document.querySelector('#particles').getBoundingClientRect().bottom < 0 && !particles_paused)
-		{
-			pJSDom[0].pJS.particles.move.enable = false;
-			particles_paused = true;
-		}
+    var gear = document.querySelector('.gear');
+    var rectGear = gear.getBoundingClientRect();
+    var centerX = rectGear.left + rectGear.width / 2;
+    var centerY = rectGear.top + rectGear.height / 2;
 
-		if (pJSDom[0] && document.querySelector('#particles').getBoundingClientRect().bottom >= 0 && particles_paused)
-		{
-			pJSDom[0].pJS.particles.move.enable = true;
-			pJSDom[0].pJS.fn.particlesRefresh();
-			particles_paused = false;
-		}
-	});
-
-	let rect = document.querySelector('#home_section .content').getBoundingClientRect();
-	document.querySelector('#home_section').style.minHeight = (rect.height + 90) + 'px';
+    document.addEventListener('mousemove', function(e) {
+        var angle = Math.atan2(e.clientY - centerY, e.clientX - centerX) * 180 / Math.PI;
+        gear.style.transform = 'rotate(' + angle + 'deg)';
+    });
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    home_animateds();
+    home_events();
+});
