8361b5057b44dcfd7fa075897661f1f373fa4e80
Matthieu Jacques <Matthieu.Jacques.2077@gmail.com>
2023-07-04 20:10:18 +0200

    Gears progress

---
+++ js/experience.js
@@ -0,0 +1,135 @@
+'use strict';
+
+function experience_events()
+{
+	let done = [];
+	let elements = [];
+
+	async function in_animation_check()
+	{
+		for (let i = 0; i < elements.length; i++)
+			if (!done[i] && is_in_viewport(elements[i]))
+			{
+				elements[i].style.opacity = '1';
+				elements[i].style.transform = 'translateY(0)';
+				done[i] = true;
+				await sleep(300);
+			}
+	}
+
+	window.addEventListener('scroll', (e) =>
+	{
+		in_animation_check();
+	});
+
+	window.addEventListener('resize', (e) =>
+	{
+		in_animation_check();
+	});
+
+	function add_job(job, inverted)
+	{
+		let tags = '';
+
+		for (let tag of job.tags)
+			tags += `<a href="${tag.url}" target="_blank">${tag.name}</a>`;
+
+		if (window.innerWidth > 780)
+		{
+			document.querySelector('#experience_section .experience_content').innerHTML += `
+				<div class="in_animation job ${inverted ? 'inverted' : ''}">
+					<div class="job_text">
+						<div class="type">
+							<span>${job.date}</span>
+							<span>•</span>
+							<span>${job.type}</span>
+						</div>
+						<a class="job_title" href="${job.link}" target="_blank">${job.title}</a>
+						<div class="text"><p>${job.description}</p></div>
+						<div class="tags">` + tags + `</div>
+					</div>
+					<div class="job_view">
+						<a ${is_safari() ? 'class="safari_fix"' : ''} href="${job.link}" target="_blank">
+							<img src="${job.image}" alt="${job.title.toLowerCase()} image" width="1440px" height="810px"/>
+							` + (job.video == 'none' || is_safari() ? '' : `
+							<div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+							<video loop muted preload="metadata">
+								<source src="${job.video}" type="video/mp4"/>
+							</video>
+							`) + `
+						</a>
+					</div>
+				</div>`;
+		}
+
+		else
+		{
+			document.querySelector('#experience_section .experience_content').innerHTML += `
+				<div class="in_animation job ${is_safari() ? 'safari_fix' : ''}" style="background-image: url(${job.image});">
+					<div class="job_text"">
+						<div class="type">
+							<span>${job.type}</span>
+							<span>•</span>
+							<span>${job.date}</span>
+						</div>
+						<a class="job_title" href="${job.link}" target="_blank">${job.title}</a>
+						<div class="text"><p>${job.description}</p></div>
+						<div class="tags">` + tags + `</div>
+					</div>
+				</div>`;
+		}
+	}
+
+	function generate(data)
+	{
+		document.querySelector('#experience_section .experience_content').innerHTML = '';
+		let inverted = data.experience.length % 2 == 0;
+
+		for (let job of data.experience)
+		{
+			inverted = !inverted;
+			add_job(job, inverted);
+		}
+
+		done = [];
+		elements = document.querySelectorAll('#experience_section .in_animation');
+
+		for (let _ of elements)
+			done.push(false);
+
+		in_animation_check();
+		videos_scroll_event();
+	}
+
+	function generate_experience()
+	{
+		read_json("resources/jsons/experience.json", generate);
+	}
+
+	let prev_width = window.innerWidth;
+	generate_experience();
+
+	window.addEventListener('resize', () =>
+	{
+		if ((prev_width >= 780 && window.innerWidth <= 780) || (prev_width <= 780 && window.innerWidth >= 780))
+		{
+			generate_experience();
+			prev_width = window.innerWidth;
+		}
+	});
+
+	document.querySelectorAll('#experience_section .sort_choices .choice').forEach((el) =>
+	{
+		el.addEventListener('click', () =>
+		{
+			generate_experience();
+
+			document.querySelectorAll('#experience_section .sort_choices .choice').forEach((el) =>
+			{
+				el.classList.remove('selected');
+			});
+
+			el.classList.add('selected');
+		});
+	});
+}
--- js/gears.js
+++ js/gears.js
@@ -1,51 +1,32 @@
 'use strict';
-function gears() {
-    var gear1 = document.querySelector('.gear-first');
-    var gear2 = document.querySelector('.gear-second');
-    
-    var lastAngle = null;
-    var lastUpdate = Date.now();
-    var ignoreRadius = 40;
-    var gearAngle = 0;
-    var targetAngle = 0; //for smoothing
-    var maxSpeed = 480;
-    var smoothing = 0.1;
+
+window.addEventListener('load', function gears() {
+    const GEAR_1_SELECTOR = '.gear-first';
+    const GEAR_2_SELECTOR = '.gear-second';
+    const IGNORE_RADIUS = 40;
+    const MAX_SPEED = 500;
+    const SMOOTHING = 0.1;
+
+    const gear1 = document.querySelector(GEAR_1_SELECTOR);
+    const gear2 = document.querySelector(GEAR_2_SELECTOR);
+
+    let gearAngle = 0;
+    let targetAngle = 0;
+    let lastAngle = null;
+    let lastUpdate = Date.now();
 
     gear1.addEventListener('mousemove', setTargetAngle);
     gear1.addEventListener('mouseleave', resetLastAngle);
 
-    // Function to set the target angle based on the mouse position
     function setTargetAngle(e) {
-        var rectGear = gear1.getBoundingClientRect();
-        var centerX = rectGear.left + rectGear.width / 2;
-        var centerY = rectGear.top + rectGear.height / 2;
-        var dx = e.clientX - centerX;
-        var dy = e.clientY - centerY;
-        var distance = Math.sqrt(dx * dx + dy * dy);
+        const { centerX, centerY } = getCenter(gear1);
+        const { distance, angle } = getDistanceAndAngle(e, centerX, centerY);
 
-        if (distance < ignoreRadius) {
-            return;
-        }
-
-        var angle = Math.atan2(dy, dx) * 180 / Math.PI;
-
+        if (distance < IGNORE_RADIUS) return;
         if (lastAngle !== null) {
-            var now = Date.now();
-            var elapsed = (now - lastUpdate) / 1000;
-            var diff = ((angle - lastAngle + 540) % 360) - 180;
-            var maxDiff = maxSpeed * elapsed;
-            if (Math.abs(diff) > maxDiff) {
-                diff = maxDiff * (diff < 0 ? -1 : 1);
-                lastAngle = null;
-                return;
-            }
-
-            // Set the target angle instead of immediately applying the rotation
-            targetAngle += diff;
-
-            lastUpdate = now;
+            adjustTargetAngle(angle);
         }
-        
+
         lastAngle = angle;
     }
 
@@ -53,23 +34,47 @@ function gears() {
         lastAngle = null;
     }
 
-    // Function to smoothly apply the rotation
+    function getCenter(element) {
+        const rect = element.getBoundingClientRect();
+        return {
+            centerX: rect.left + rect.width / 2,
+            centerY: rect.top + rect.height / 2,
+        };
+    }
+
+    function getDistanceAndAngle(e, centerX, centerY) {
+        const dx = e.clientX - centerX;
+        const dy = e.clientY - centerY;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+        const angle = Math.atan2(dy, dx) * 180 / Math.PI;
+
+        return { distance, angle };
+    }
+
+    function adjustTargetAngle(angle) {
+        const elapsed = (Date.now() - lastUpdate) / 1000;
+        let diff = ((angle - lastAngle + 540) % 360) - 180;
+        const maxDiff = MAX_SPEED * elapsed;
+
+        if (Math.abs(diff) > maxDiff) {
+            diff = maxDiff * (diff < 0 ? -1 : 1);
+            lastAngle = null;
+            return;
+        }
+
+        targetAngle += diff;
+        lastUpdate = Date.now();
+    }
+
     function applyRotation() {
-        // Calculate the difference between the target angle and the current angle
-        var angleDiff = targetAngle - gearAngle;
+        const angleDiff = targetAngle - gearAngle;
+        gearAngle += angleDiff * SMOOTHING;
 
-        // Apply a fraction of the difference to the current angle
-        gearAngle += angleDiff * smoothing;
+        gear1.style.transform = `rotate(${gearAngle}deg)`;
+        gear2.style.transform = `rotate(${-gearAngle * 1.25}deg)`;
 
-        gear1.style.transform = 'rotate(' + gearAngle + 'deg)';
-        gear2.style.transform = 'rotate(' + -gearAngle * 1.25 + 'deg)';
-
-        // Continue applying the rotation in the next frame
         requestAnimationFrame(applyRotation);
     }
 
-    // Start applying the rotation
     applyRotation();
-}
-
-window.addEventListener('load', gears);
+});
