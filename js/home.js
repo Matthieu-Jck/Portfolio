2c4b51131eeb1733548041715dfd0fe57a569f98
Matthieu Jacques <Matthieu.Jacques.2077@gmail.com>
2023-07-07 14:39:30 +0200

    minor changes, might not be useful

--- js/home.js
+++ js/home.js
@@ -46,15 +46,16 @@ var makeItRain = function () {
     var drops = "";
     var backDrops = "";
 
-    while (increment < 100) {
-        //couple random numbers to use for various randomizations
-        //random number between 98 and 1
+    // Get screen width
+    var screenWidth = $(window).width();
+
+    // Set the number of raindrops based on screen width
+    var numOfDrops = screenWidth > 800 ? 80 : 40; // Reduce the number for smaller screens
+
+    while (increment < numOfDrops) {
         var randoHundo = (Math.floor(Math.random() * (98 - 1 + 1) + 1));
-        //random number between 5 and 2
         var randoFiver = (Math.floor(Math.random() * (5 - 2 + 1) + 2));
-        //increment
         increment += randoFiver;
-        //add in a new raindrop with various randomizations to certain CSS properties
         drops += '<div class="drop" style="left: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
         backDrops += '<div class="drop" style="right: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
     }
